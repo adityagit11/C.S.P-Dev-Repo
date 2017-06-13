68e9078d0cad77b7e8f44132c30b58cba3a6cdf8
Aditya Singh <adityasinghwolvieait@gmail.com>
2017-06-13 10:46:16 +0530

    Supposedly Final Commit

---
+++ CSP/src/org/edelweiss/csp/test/ReduceToLowerCase.java
@@ -0,0 +1,65 @@
+package org.edelweiss.csp.test;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ReduceToLowerCase 
+{
+	//file paths are empty
+	public static String FilePathToRead = "E:\\JavaTech\\DataRepo\\GlobalSentenceDataSet.txt";
+	public static String FilePathToWrite = "E:\\JavaTech\\DataRepo\\LocalSentenceDataSet.txt";
+	public static void main(String[] args)
+	{
+		try
+		{
+			String eachLine = "";
+			FileReader dataset = new FileReader(FilePathToRead);
+			BufferedReader readDataLine = new BufferedReader(dataset);
+			while((eachLine = readDataLine.readLine()) != null)
+			{
+				write(eachLine);
+			}
+			readDataLine.close();
+			dataset.close();
+		}
+		catch(FileNotFoundException e)
+		{
+			System.err.println("Unable to find the file");
+		}
+		catch(IOException e)
+		{
+			System.err.println("Unable to read the file");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public static void write(String sentence)
+	{
+		sentence = sentence.toLowerCase();
+		try
+		{
+			FileWriter fw = new FileWriter(FilePathToWrite, true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(sentence + "\n");
+			bw.close();
+		}
+		catch(FileNotFoundException e)
+		{
+			System.err.println("Unable to find the file");
+		}
+		catch(IOException e)
+		{
+			System.err.println("Unable to read the file");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
